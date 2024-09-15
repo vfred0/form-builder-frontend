ccf6a0f06bc8dc75ac9825cd0938f4b54111bfd9
Víctor Arreaga <arreagav17@gmail.com>
2024-09-15 17:16:32 -0500

    Implements form structure service and page

--- src/app/pages/form-structure-management/form-structure-management.component.ts
+++ src/app/pages/form-structure-management/form-structure-management.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormStructureService} from "@shared/services/form-structure.service";
+import {
+    NbButtonModule,
+    NbCardModule,
+    NbDialogService,
+    NbIconModule,
+    NbLayoutModule,
+    NbListModule,
+    NbTagModule
+} from "@nebular/theme";
+import {FormStructureResponseDto} from "@core/dtos/form-structure/form-structure-response.dto";
+import {NgStyle} from "@angular/common";
+import {
+    FormFormStructureComponent
+} from "@pages/form-structure-management/form-form-structure/form-form-structure.component";
 
 @Component({
-  selector: 'form-builder-form-structure-management',
-  standalone: true,
-  imports: [],
-  templateUrl: './form-structure-management.component.html',
-  styles: ``
+    selector: 'form-builder-form-structure-management',
+    standalone: true,
+    imports: [NbLayoutModule, NbCardModule, NbTagModule, NbListModule, NgStyle, NbButtonModule, NbIconModule],
+    templateUrl: './form-structure-management.component.html',
+    styles: ``
 })
 export class FormStructureManagementComponent {
+    protected formStructures: FormStructureResponseDto[] = [];
+    protected dialogService = inject(NbDialogService);
+    private readonly formStructureService = inject(FormStructureService);
+    private data: any;
 
+    constructor() {
+        this.formStructureService.getAll().subscribe(formStructures => this.formStructures = formStructures);
+    }
+
+    showFormStructureModal() {
+        this.dialogService.open(FormFormStructureComponent)
+            .onClose.subscribe( values => { console.log(values); });
+    }
 }
+
---
+++ src/app/shared/services/form-structure.service.ts
@@ -0,0 +1,45 @@
+import {inject, Injectable} from "@angular/core";
+import {environment} from '@env/environment.development';
+import {HttpClient} from "@angular/common/http";
+import {FormStructureResponseDto} from "@core/dtos/form-structure/form-structure-response.dto";
+import {map} from "rxjs";
+import {FormStructureInputRequest} from "@core/dtos/form-structure/form-structure-input.request";
+
+@Injectable({providedIn: 'root'})
+export class FormStructureService {
+    private readonly API_URL = environment.apiBaseUrl + 'form-structures';
+    private http: HttpClient = inject(HttpClient);
+
+    getAll() {
+        return this.http.get<{ data: FormStructureResponseDto[] }>(this.API_URL).pipe(map(response => response.data));
+    }
+
+    getById(id: number) {
+        return this.http.get<{
+            data: FormStructureResponseDto
+        }>(`${this.API_URL}/${id}`).pipe(map(response => response.data));
+    }
+
+    save(formStructure: FormStructureResponseDto) {
+        return this.http.post<{
+            data: FormStructureResponseDto
+        }>(this.API_URL, formStructure).pipe(map(response => response.data));
+    }
+
+    update(formStructure: FormStructureResponseDto, id: number) {
+        return this.http.put<{
+            data: FormStructureResponseDto
+        }>(`${this.API_URL}/${id}`, formStructure).pipe(map(response => response.data));
+    }
+
+    delete(id: number) {
+        return this.http.delete<{ data: any }>(`${this.API_URL}/${id}`).pipe(map(response => response.data));
+    }
+
+    setInputs(formStructure: FormStructureInputRequest) {
+        return this.http.post<{
+            data: any
+        }>(`${this.API_URL}/${formStructure.formStructureId}/inputs`, formStructure.inputsId).pipe(map(response => response.data));
+    }
+
+}
