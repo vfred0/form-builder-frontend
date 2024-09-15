b264f8a83c90fa1199dc59bfcbd92ee3ea0343cd
Víctor Arreaga <arreagav17@gmail.com>
2024-09-15 15:14:36 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+import {
+    NbActionsModule,
+    NbButtonModule,
+    NbCardModule,
+    NbLayoutModule,
+    NbMenuModule,
+    NbSidebarModule,
+    NbTabsetModule,
+    NbToggleModule
+} from "@nebular/theme";
 
 @Component({
-  selector: 'form-builder-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
+    selector: 'form-builder-root',
+    standalone: true,
+    imports: [RouterOutlet, NbCardModule, NbTabsetModule, NbLayoutModule, NbActionsModule, NbMenuModule, NbSidebarModule, NbToggleModule, NbButtonModule],
+    template: `
+        <nb-layout>
+            <nb-layout-header>
+                <h1>Form Builder</h1>
+            </nb-layout-header>
 
-    <router-outlet />
-  `,
-  styles: [],
+            <nb-layout-column>
+                <nb-tabset>
+                    <nb-tab tabTitle="Gestionar estructura de formularios">
+                        Gestionar estructura de fomrularios
+                    </nb-tab>
+                    <nb-tab tabTitle="Gestionar inputs">
+                        Gestionar inputs
+                    </nb-tab>
+                </nb-tabset>
+            </nb-layout-column>
+        </nb-layout>
+    `,
+    styles: [],
 })
+
+
 export class AppComponent {
-  title = 'form-builder-frontend';
+    title = 'form-builder-frontend';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, EnvironmentProviders, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {NbDialogModule, NbSidebarModule, NbThemeModule, NbToastrModule} from "@nebular/theme";
+import {NbEvaIconsModule} from "@nebular/eva-icons";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+const provideNebular = (): EnvironmentProviders[] => [importProvidersFrom(NbThemeModule.forRoot({name: 'cosmic'})), importProvidersFrom(NbEvaIconsModule), importProvidersFrom(NbDialogModule.forRoot()), importProvidersFrom(NbSidebarModule.forRoot()), importProvidersFrom(NbToastrModule.forRoot())]
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+    providers: [provideRouter(routes), importProvidersFrom(BrowserAnimationsModule), ...provideNebular()],
 };
