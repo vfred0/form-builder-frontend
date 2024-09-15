f92cd4d16de3dee88f4f00ba083f7124075cebaa
Víctor Arreaga <arreagav17@gmail.com>
2024-09-15 17:17:39 -0500

    Added HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,11 @@ import {routes} from './app.routes';
 import {NbDialogModule, NbSidebarModule, NbThemeModule, NbToastrModule} from "@nebular/theme";
 import {NbEvaIconsModule} from "@nebular/eva-icons";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
 
 const provideNebular = (): EnvironmentProviders[] => [importProvidersFrom(NbThemeModule.forRoot({name: 'cosmic'})), importProvidersFrom(NbEvaIconsModule), importProvidersFrom(NbDialogModule.forRoot()), importProvidersFrom(NbSidebarModule.forRoot()), importProvidersFrom(NbToastrModule.forRoot())]
 
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), importProvidersFrom(BrowserAnimationsModule), ...provideNebular()],
+    providers: [provideRouter(routes), importProvidersFrom(HttpClientModule, BrowserAnimationsModule), ...provideNebular()],
 };
